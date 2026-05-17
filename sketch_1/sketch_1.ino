c8b8feff81a66de39fc2be4a20730ea6edc8f785
William Y. Feng <womogenes2@gmail.com>
2026-05-16 21:07:42 -0700

    Fix off by one issue

--- sketch_1/sketch_1.ino
+++ sketch_1/sketch_1.ino
@@ -7,14 +7,14 @@ const int CLK_AN = 14;
 int counter = 0;
 
 int image[8] = {
+  0b00000000,
+  0b01000010,
+  0b10100101,
   0b11111111,
-  0b11111110,
-  0b11111100,
-  0b11111000,
-  0b11110000,
-  0b00000000,
-  0b00000000,
-  0b00000000,
+  0b10000001,
+  0b10100101,
+  0b10000001,
+  0b01111110,
 };
 
 void setup() {
@@ -38,9 +38,6 @@ void pulseCol() {
 void displayImage(int arr[8]) {
   // Put 0 into first row, 1 for the next 7
   for (int row = 0; row < 8; row++) {
-    digitalWrite(SER_AN, row == 0 ? LOW : HIGH);
-    pulseRow();
-
     // Shift in the column
     for (int col = 0; col < 8; col++) {
       digitalWrite(SER_CAT, arr[row] & (1 << col) ? HIGH : LOW);
@@ -48,9 +45,11 @@ void displayImage(int arr[8]) {
     }
     pulseCol();
 
-    delay(1);
+    digitalWrite(SER_AN, row == 7 ? LOW : HIGH);
+    pulseRow();
+
+    delay(2);
   }
-  pulseRow();
 }
 
 void loop() {
